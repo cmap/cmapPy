b5fb5265f92d43dccbf39e0e314413cc995e3674
Oana Enache <oamenache@gmail.com>
2017-03-31 13:48:35 -0400

    added parse to pandasgexpress __init__; initial setup method

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-	name="broadinstitute_cmap",
-	packages=["broadinstitute_cmap"]
+setup(name="cmapPy",
+      version = "1.0.0",
+      description = "Utility methods for interacting with .gctx and .gct files, and other resources useful to Connectivity Map users",
+      url="https://github.com/cmap/cmapPy",
+      author="Oana Enache, Lev Litichevskiy, David Lahr",
+      author_email="oana@broadinstitute.org", 
+      license = "MIT 3-clause",
+      packages = ["cmapPy"],
+      zip_safe = False
 )
